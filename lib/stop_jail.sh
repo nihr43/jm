dcd85dbeffae84b60611450872d576d5daea1ce7
Charlie Root <root@jm.localdomain>
2018-04-05 22:27:52 +0000

    umounting works

--- lib/stop_jail.sh
+++ lib/stop_jail.sh
@@ -9,4 +9,8 @@ JAIL_NAME="$2"
 service jail stop $JAIL_NAME
 #jail -f $JAIL_DIR/$JAIL_NAME/.jail.conf -r $JAIL_NAME
 
+umount $JAIL_DIR/$JAIL_NAME/$JAIL_NAME/dev/fd
+umount $JAIL_DIR/$JAIL_NAME/$JAIL_NAME/dev
+umount $JAIL_DIR/$JAIL_NAME/$JAIL_NAME/proc
+
 rm $JAIL_DIR/$JAIL_NAME/lock
